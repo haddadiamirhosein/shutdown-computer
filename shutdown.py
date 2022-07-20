bcb4ce21d6fd3a87a0d6d499bd903e97d64d340c
haddadiamirhosein <haddadiamirhosein.d@gmail.com>
2022-07-20 14:55:41 +0430

    enable to close and computer dont shutdown

--- shutdown.py
+++ shutdown.py
@@ -8,15 +8,13 @@ s = int(input("Enter second : "))
 hour = h*3600
 minute = m*60
 
-time = str(hour+minute+s)
+time = hour+minute+s
 
-os.system("shutdown /s /t "+time)
-
-time = int(time)
 for i in range(time,0,-1):
     os.system("cls")
     h = str(int(i / 3600))
     m = str(int((i%3600)/60))
     s = str(int((i%3600)%60))
-    print(h+" : "+m+" : "+s)
+    print("time to shutdown : "+h+" : "+m+" : "+s)
     ti.sleep(1)
+os.system("shutdown /s /t 1")
